d9f345dac866ec1f45135dae231a788e15ade6c6
Chris Brownell <42624201+chrisbrownell@users.noreply.github.com>
2018-09-09 17:37:30 -0400

    Add files via upload

---
+++ Lab2-Encoder-Test.ino
@@ -0,0 +1,60 @@
+/* Rotary encoder test code printing to LCD screen
+ * Modified from https://github.com/FAR-Lab/Developing-and-Designing-Interactive-Devices/wiki/Rotary-Encoder-test-Code by Jessica Faruque
+ * Modified by Chris Brownell 9/9/2018
+ * 
+ */
+ 
+#include <LiquidCrystal.h>
+#define ENC_A 7 //these need to be digital input pins
+#define ENC_B 8
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+ 
+void setup()
+{
+  /* Setup encoder pins as inputs */
+  pinMode(ENC_A, INPUT_PULLUP);
+  pinMode(ENC_B, INPUT_PULLUP);
+ 
+  Serial.begin (9600);
+  Serial.println("Start");
+  lcd.begin(16, 2);
+}
+ 
+void loop()
+{
+  static unsigned int counter4x = 0;      //the SparkFun encoders jump by 4 states from detent to detent
+  static unsigned int counter = 0;
+  static unsigned int prevCounter = 0;
+  int tmpdata;
+  tmpdata = read_encoder();
+  if( tmpdata) {
+    counter4x += tmpdata;
+    counter = counter4x/4;
+    if (prevCounter != counter){
+      lcd.clear();
+      Serial.print("Counter value: ");
+      Serial.println(counter);
+      lcd.print("Counter: ");
+      lcd.print(counter);
+    }
+    prevCounter = counter;
+  }
+}
+ 
+/* returns change in encoder state (-1,0,1) */
+int read_encoder()
+{
+  static int enc_states[] = {
+    0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0  };
+  static byte ABab = 0;
+  ABab *= 4;                   //shift the old values over 2 bits
+  ABab = ABab%16;      //keeps only bits 0-3
+  ABab += 2*digitalRead(ENC_A)+digitalRead(ENC_B); //adds enc_a and enc_b values to bits 1 and 0
+  return ( enc_states[ABab]);
+ 
+ 
+}
